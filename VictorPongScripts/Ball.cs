45df8ee30c024bfbb3c6fc56185178c77082a4ff
Victor Pinheiro <vpinheiro.aguiar@gmail.com>
2017-11-14 20:46:55 -0200

    Teste para commit

---
+++ VictorPongScripts/Ball.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Ball : MonoBehaviour {
+
+	private float velBX, velBY;
+	public float speed;
+	private float randomAngle;
+	private Rigidbody2D ballRb;
+	private Transform ballTr;
+	public GameObject ballPrefab;
+	public Vector3 pos;
+
+	private Rigidbody2D compBarRb;
+	private int directBar;
+
+	private Text score1;
+	private Text score2;
+
+	private PlayerBar playerSc;
+	private Rigidbody2D playerBarRb;
+
+	void Start () {
+		ballRb = GetComponent<Rigidbody2D> ();
+		ballTr = GetComponent<Transform> ();
+
+		compBarRb = GameObject.Find ("CompBar").GetComponent<Rigidbody2D> ();
+
+		playerSc = GameObject.Find ("playerBar").GetComponent<PlayerBar> ();
+		playerBarRb = GameObject.Find ("playerBar").GetComponent<Rigidbody2D> ();
+
+		score1 = GameObject.Find ("Score1").GetComponent<Text> ();
+		score2 = GameObject.Find ("Score2").GetComponent<Text> ();
+
+		pos = ballTr.position;
+
+		speed = 8f;
+
+		randomAngle = getRandonAngle (true);
+		velBX = Mathf.Cos (randomAngle) * speed;
+		velBY = Mathf.Sin (randomAngle) * speed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		ballRb.velocity = new Vector2 (velBX, velBY);
+
+		if (ballRb.position.y < compBarRb.position.y)
+			directBar = -1;
+		else
+			directBar = 1;
+		
+		if (ballRb.position.y > compBarRb.position.y + 0.64 || ballRb.position.y < compBarRb.position.y - 0.64)
+			compBarRb.velocity = new Vector2 (compBarRb.velocity.x, directBar * 7);
+		else if (ballRb.position.y < compBarRb.position.y + 0.64 && ballRb.position.y > compBarRb.position.y - 0.64)
+			compBarRb.velocity = new Vector2 (compBarRb.velocity.x, 0);
+
+		if (ballRb.position.x > compBarRb.position.x || ballRb.position.x < playerBarRb.position.x)
+			over ();
+	}
+
+	void bounceBall() {
+		int direct = (ballRb.position.x > 0)?( -1):(1);
+
+		randomAngle = getRandonAngle (false);
+
+		float dir = Random.value;
+		if (dir > 0.5)
+			dir = 1;
+		else
+			dir = -1;
+		
+		velBX = direct * Mathf.Cos(randomAngle) * speed * 1.3f;
+		velBY = dir * Mathf.Sin (randomAngle) * speed * 1.3f;
+	}
+
+	void OnCollisionEnter2D (Collision2D colisao) {
+		if (colisao.gameObject.tag == "parede") {
+			velBY *= -1;
+		}
+		if (colisao.gameObject.tag == "bar") {
+			bounceBall ();
+		}
+	}
+
+	float getRandonAngle(bool init) {
+		float angle;
+		if (init) {
+			angle = Random.value * 2 * Mathf.PI;
+			while ((angle > Mathf.PI / 3 && angle < 2 * Mathf.PI / 3) || (angle > 4 * Mathf.PI / 3 && angle < 5 * Mathf.PI / 3))
+				angle = Random.value * 2 * Mathf.PI;
+		} else {
+			angle = (Random.value * Mathf.PI / 3);
+		}
+
+		return angle;
+	}
+
+	void over() {
+		if (ballRb.position.x > 0) {
+			playerSc.scr1++;
+			score1.text = "" + playerSc.scr1;
+		} else {
+			playerSc.scr2++;
+			score2.text = "" + playerSc.scr2;
+		}
+
+		Instantiate (ballPrefab, pos, transform.localRotation);
+		Destroy (this.gameObject);
+	}
+
+}
---
+++ VictorPongScripts/PlayerBar.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBar : MonoBehaviour {
+
+	private Rigidbody2D barRb;
+	public int velY = 0;
+	private int direct = 0;
+	private bool upPressed = false, downPressed = false;
+
+	private Rigidbody2D ballRb;
+
+	public int scr1 = 0, scr2 = 0;
+
+	void Start () {
+		barRb = GetComponent<Rigidbody2D> ();	
+		barRb.velocity = new Vector2(barRb.velocity.x, velY);
+
+		ballRb = GameObject.FindWithTag ("ball").GetComponent<Rigidbody2D> ();
+	}
+
+	void Update () {
+		if (Input.touchCount > 0 && Input.GetTouch (0).phase == TouchPhase.Moved) {
+			Vector2 touchPosition = Input.GetTouch (0).position;
+
+			if (touchPosition.y > 0) {
+				upPressed = true;
+			} else
+				upPressed = false;
+			if (touchPosition.y <= 0) {
+				downPressed = true;
+			} else
+				downPressed = false;
+		}
+
+		ballRb = GameObject.FindWithTag ("ball").GetComponent<Rigidbody2D> ();
+
+		if (Input.GetKeyDown (KeyCode.UpArrow)) {
+			upPressed = true;
+		}
+		if (Input.GetKeyDown (KeyCode.DownArrow)) {
+			downPressed = true;
+		}
+		if (Input.GetKeyUp (KeyCode.UpArrow)) {
+			upPressed = false;
+		} else if (Input.GetKeyUp (KeyCode.DownArrow)) {
+			downPressed = false;
+		}
+
+		if (upPressed)
+			velY = 7;
+		else if (downPressed)
+			velY = -7;
+		else
+			velY = 0;
+
+		barRb.velocity = new Vector2(barRb.velocity.x, velY);
+	}
+}
